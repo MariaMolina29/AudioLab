8db210166db49dfb03ab0b439b94e1ab1d11206c
Andres Oñate <andresfelipeonategonzalez@gmail.com>
2024-10-31 09:25:45 -0500

    first commit

---
+++ app.py
@@ -0,0 +1,213 @@
+from flask import Flask, render_template, session, send_file, request, jsonify
+from flask_socketio import SocketIO
+import redis
+import uuid
+import numpy as np
+import parselmouth
+from audio_analysis import analyze_audio
+import io
+from scipy.io import wavfile
+from datetime import timedelta
+
+
+
+redis_client = redis.StrictRedis(host='localhost', port=6379, db=0)
+app = Flask(__name__)
+app.secret_key = 'trabajo_de_grado_12601907' 
+app.config.update(
+    SESSION_COOKIE_SECURE=True,  # Cookies solo se envían por HTTPS
+    SESSION_COOKIE_HTTPONLY=True,  # Cookies inaccesibles desde JavaScript
+    SESSION_COOKIE_SAMESITE='Lax',  # Evitar envío en solicitudes cruzadas
+    PERMANENT_SESSION_LIFETIME=timedelta(minutes=30),  # Tiempo de vida de la sesión (30 min)
+)
+
+socket_io = SocketIO(app)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/index.html')
+def index2():
+    return render_template('index.html')
+
+@app.route('/real_time_analysis.html')
+def real_timeAnalysis():
+    return render_template('real_time_analysis.html')
+
+@app.route('/wav_analysis.html')
+def wav_Analysis():
+    return render_template('wav_analysis.html')
+
+@app.route('/manual.html')
+def manual():
+    return render_template('manual.html')
+
+@app.route('/about.html')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/verificar_sesion', methods=['POST'])
+def verificar_sesion():
+    # Verifica si ya existe un user_id en la sesión
+    if 'user_id' in session:
+        return jsonify({"usuario_creado": True})
+    else:
+        return jsonify({"usuario_creado": False})
+
+@app.route('/crear_sesion', methods=['POST'])
+def crear_sesion():
+    # Crea una sesión si no existe y el usuario acepta cookies
+    if 'user_id' not in session:
+        session['user_id'] = str(uuid.uuid4())
+        return jsonify({"mensaje": "Sesión creada"})
+    else:
+        return jsonify({"mensaje": "Sesión ya existe"})
+
+
+@socket_io.on('disconnect')
+def handle_disconnect():
+    user_id = session.get('user_id')
+    if user_id:
+        key_audio_data = f'{user_id}_audio_data'
+        key_data_queue = f'{user_id}_data_queue'
+        key_sample_rate= f'{user_id}_sample_rate'
+        key_audio_data_copy = f'{user_id}_audio_data_copy'
+        key_sample_rate_copy= f'{user_id}_sample_rate_copy'
+
+        if redis_client.exists(key_audio_data):
+            redis_client.delete(key_audio_data)
+            redis_client.delete(key_data_queue)
+            redis_client.delete(key_sample_rate)
+        elif redis_client.exists(key_audio_data_copy):
+            redis_client.delete(key_audio_data_copy)
+            redis_client.delete(key_sample_rate_copy)
+
+
+@socket_io.on('start_recording')
+def handle_start_recording():
+    user_id = session.get('user_id')
+    if not user_id:
+        socket_io.emit('handle_complete', {'message': 'Clave procesada'})
+        return
+    key_audio_data = f'{user_id}_audio_data'
+    key_data_queue = f'{user_id}_data_queue'
+    key_sample_rate= f'{user_id}_sample_rate'
+    key_audio_data_copy = f'{user_id}_audio_data_copy'
+    key_sample_rate_copy= f'{user_id}_sample_rate_copy'
+    if redis_client.exists(key_audio_data):
+        redis_client.delete(key_audio_data)
+        redis_client.delete(key_data_queue)
+        redis_client.delete(key_sample_rate)
+        redis_client.delete(key_audio_data_copy)
+        redis_client.delete(key_sample_rate_copy)
+
+    socket_io.emit('handle_complete', {'message': 'Clave procesada'})
+   
+@socket_io.on('set_sample_rate')
+def handle_set_sample_rate(data):
+    user_id = session.get('user_id')
+    if not user_id:
+        return  
+    sample_rate = data.get('sample_rate')
+    if sample_rate:
+        redis_client.set(f'{user_id}_sample_rate', sample_rate)
+        print(f"Frecuencia de muestreo {sample_rate} almacenada para el usuario {user_id}")
+
+@socket_io.on('audio_data')
+def handle_audio_data(data):
+    user_id = session.get('user_id')
+    if not user_id:
+        return
+    redis_client.rpush(f'{user_id}_audio_data', data)
+    redis_client.rpush(f'{user_id}_data_queue', data)
+    # Limitar el tamaño de la cola (buffersize)
+    buffer_size = 5
+    data_queue_length = redis_client.llen(f'{user_id}_data_queue')
+    if data_queue_length > buffer_size:
+        redis_client.ltrim(f'{user_id}_data_queue', data_queue_length - buffer_size, -1)
+
+    key_data_queue = f'{user_id}_data_queue'
+    key_sample_rate= f'{user_id}_sample_rate'
+    audio_data_queue = redis_client.lrange(key_data_queue, 0, -1)  # Obtener todos los elementos de `data_queue`
+    sample_rate = int(redis_client.get(key_sample_rate))
+
+    if len(audio_data_queue) > 0 and sample_rate:
+        # Convertir los datos de audio en un array de numpy
+        combined_audio_queue = np.concatenate([(np.frombuffer(chunk, dtype=np.float32)) for chunk in audio_data_queue])
+        combined_audio_queue = (combined_audio_queue * 32767).astype(np.int16)  
+        sound_real_time = parselmouth.Sound(combined_audio_queue, sampling_frequency=sample_rate)
+        data_to_send_real_time= analyze_audio(sound_real_time, True)
+        socket_io.emit('plot_data_real_time', data_to_send_real_time)
+
+@socket_io.on('process_wav')
+def handle_process_wav(data):
+    audio_file = io.BytesIO(data)
+    sample_rate, audio_data_wav = wavfile.read(audio_file)
+    # Asegurarse de que los datos sean unidimensionales 
+    if len(audio_data_wav.shape) > 1:
+        audio_data_wav = audio_data_wav.mean(axis=1)  # Convertir a mono si es estéreo
+
+    sound_wav = parselmouth.Sound(values=audio_data_wav, sampling_frequency=sample_rate)
+    data_to_send_wav  = analyze_audio(sound_wav, False)
+    socket_io.emit('plot_data_wav', data_to_send_wav)
+
+@socket_io.on('save_data')
+def handle_process_real_time():
+    user_id = session.get('user_id')
+    key_audio_data = f'{user_id}_audio_data'
+    key_sample_rate= f'{user_id}_sample_rate'
+    key_audio_data_copy = f'{user_id}_audio_data_copy'
+    key_sample_rate_copy= f'{user_id}_sample_rate_copy'
+
+    if redis_client.exists(key_audio_data):
+        try :
+            audio_data = redis_client.lrange(key_audio_data, 0, -1)
+            sample_rate = redis_client.get(key_sample_rate)
+
+            for chunk in audio_data:
+                redis_client.rpush(key_audio_data_copy, chunk)
+
+            redis_client.set(key_sample_rate_copy, sample_rate)
+
+            socket_io.emit('handle_complete', {'message': 'Audio procesado'})
+        except:
+            redis_client.delete(key_audio_data_copy)
+            redis_client.delete(key_sample_rate_copy)
+            socket_io.emit('handle_complete')
+    else:
+        socket_io.emit('handle_complete')
+
+@socket_io.on('get_processed_audio')
+def handle_get_processed_audio():
+    user_id = session.get('user_id')
+    key_audio_data_copy = f'{user_id}_audio_data_copy'
+    key_sample_rate_copy= f'{user_id}_sample_rate_copy'
+
+
+    if redis_client.exists(key_audio_data_copy):
+        socket_io.emit('loading')
+        audio_data = redis_client.lrange(key_audio_data_copy, 0, -1) 
+        sample_rate = int(redis_client.get(key_sample_rate_copy))
+
+        combined_audio = np.concatenate([(np.frombuffer(chunk, dtype=np.float32)) for chunk in audio_data])
+        combined_audio = (combined_audio * 32767).astype(np.int16)  
+        audio_file = io.BytesIO()
+        wavfile.write(audio_file, sample_rate, combined_audio)
+        audio_file.seek(0)
+        sound_save_audio = parselmouth.Sound(combined_audio, sampling_frequency=sample_rate)
+        data_to_send_save_audio= analyze_audio(sound_save_audio, False)
+
+        data_to_send_auido_data = {
+            'plot_data': data_to_send_save_audio,  
+            'audio': audio_file.read()  
+        }
+        redis_client.delete(key_audio_data_copy)
+        redis_client.delete(key_sample_rate_copy)
+        socket_io.emit('plot_save_audio', data_to_send_auido_data)
+
+
+if __name__ == '__main__':
+    socket_io.run(app, debug=True)
---
+++ audio_analysis.py
@@ -0,0 +1,413 @@
+import numpy as np
+import plotly.graph_objs as go
+from scipy.signal import savgol_filter
+from parselmouth.praat import call
+import io
+import json
+
+
+def draw_spectrogram_3d(spectrogram):
+    X, Y = np.meshgrid(spectrogram.xs(), spectrogram.ys())
+    Z = 10 * np.log10(spectrogram.values + 1e-16)
+
+    trace_spectrogram_3d = [{
+        'z': Z.tolist(),
+        'x': X.tolist(),
+        'y': Y.tolist(),
+        'type': 'surface',  
+        'colorscale': 'Hot',
+        'colorbar': {
+            'title': "Intensidad [dB]",
+            'thickness': 10,
+            'titleside': 'right',
+            'titlefont': {'size': 12},
+            'tickmode': 'auto',
+            'ticks': 'outside'
+        },
+        'hovertemplate': 'Tiempo: %{x: .2f} s<br>Frecuencia: %{y: .2f} Hz<br>Intensidad: %{z: .3f} dB<extra></extra>'
+
+    }]
+
+    layout_spectrogram_3d= {
+    'title': "Espectrograma 3D",
+    'scene': {
+        'xaxis': {'title': "Tiempo [s]"},
+        'yaxis': {'title': "Frecuencia [Hz]", 'zeroline': False, 'range': [0, 8000]},
+        'zaxis': {'title': "Intensidad [dB]", 'range': [np.min(Z), np.max(Z)]},
+        'camera': {'eye': {'x': 2, 'y': -2, 'z': 1}},
+        'dragmode': False 
+    },
+    'hovermode': 'closest',
+    'autosize': True,
+    # 'margin': {'l': 50, 'r': 50, 't': 50, 'b': 50}
+    }
+
+    return trace_spectrogram_3d, layout_spectrogram_3d
+ 
+def draw_spectrogram(spectrogram, pitch_values, time_pitch, formants):
+    X, Y = np.meshgrid(spectrogram.xs(), spectrogram.ys())
+    Z = 10 * np.log10(spectrogram.values + 1e-16)
+
+    trace_spectrogram = [{
+        'z': Z.tolist(),  
+        'x': X[0].tolist(),  
+        'y': Y[:, 0].tolist(),  
+        'type': 'heatmap',
+        'colorscale': 'Hot',
+        'zmin': np.min(Z), 
+        'zmax': np.max(Z), 
+        'name': "Espectrograma",
+        'colorbar': {
+            'title': "Intensidad [dB]",
+            'thickness': 10,  
+            'titleside': 'right',  
+            'titlefont': {'size': 12}, 
+            'tickmode': 'auto',  
+            'ticks': 'outside' 
+        },
+        'hovertemplate': 'Tiempo: %{x: .2f} s<br>Frecuencia: %{y: .2f} Hz<br>Intensidad: %{z: .3f} dB'
+    }]
+
+    spectrogram_data = {
+        'times': spectrogram.xs().tolist(),
+        'frequencies': spectrogram.ys().tolist(),
+        'power_values': Z.tolist()  # Matriz de potencias
+    }
+    
+
+    # Aplicar suavizado a la curva de la frecuencia fundamental
+    # pitch_values = savgol_filter(pitch_values, window_length=11, polyorder=2)
+   
+    trace_pitch = [{
+        'x': time_pitch.tolist(),  # Convertir array time_pitch a lista para JSON serializable
+        'y': pitch_values.tolist(),  # Convertir array pitch_values a lista para JSON serializable
+        'mode': 'lines+markers',
+        'marker': {'size': 3, 'color': 'cyan'},
+        'line': {'color': 'cyan'},
+        'name': "Frecuencia Fundamental",
+        'showlegend': True,
+        'hovertemplate': 'Tiempo: %{x: .2f} s<br>Frecuencia: %{y: .2f} Hz'
+
+    }]
+    
+    traces = trace_spectrogram  + trace_pitch
+    formants_values = []
+
+   
+    colors = ['olivedrab', 'dodgerblue', 'lightgray'] 
+    for formant_number in range(1, 4):  # Los primeros 3 formantes
+        # formant_values = np.array([np.nan if np.isnan(p) else formants.get_value_at_time(formant_number, t)
+        #                for p, t in zip(pitch_values, time_pitch)])
+        formant_values = np.array([0 if  p==0 else formants.get_value_at_time(formant_number, t)
+                        for p, t in zip(pitch_values, time_pitch)])
+        formant_values = np.nan_to_num(formant_values, nan=0)
+        trace_formant = [{
+            'x': time_pitch.tolist(),  # Convertir array time_pitch a lista para JSON serializable
+            'y': formant_values.tolist(),  # Convertir array formant_values a lista para JSON serializable
+            'mode': 'lines+markers',
+            'marker': {'size': 2},
+            'line': {'dash': 'dash', 'color': colors[formant_number - 1] },
+            'name': f"Formante {formant_number}",
+            'hovertemplate': 'Tiempo: %{x: .2f} s<br>Frecuencia: %{y: .2f} Hz'
+        }]
+        formants_values.append(formant_values)
+        traces = traces + trace_formant
+
+    layout = {
+        'title': "Espectrograma",
+        'xaxis': {'title': "Tiempo [s]"},
+        'yaxis': {'title': "Frequencia [Hz]", 'range': [0, 8000]},
+        'hovermode': 'closest',
+        'legend': {
+            'x': 0.5,
+            'y': -0.2,
+            'xanchor': 'center',
+            'yanchor': 'top',
+            'orientation': 'h',
+            'font': {'size': 10}
+        },
+        'font': {
+            'family': 'Helvetica Neue', 
+        },
+        # 'margin': {'l': 50, 'r': 50, 't': 50, 'b': 50},  # Margin en una sola línea
+        'autosize': True,
+        'dragmode': False 
+
+    }
+
+ 
+    return traces, layout, formants_values, spectrogram_data
+ 
+def draw_combined_pitch_intensity_contour(pitch_values, time_pitch, intensity):
+ 
+    # Traza de la frecuencia fundamental (Pitch)
+    trace_pitch = [{
+        'x': time_pitch.tolist(),  # Convertir a lista para ser serializable
+        'y': pitch_values.tolist(),  # Convertir a lista para ser serializable
+        'mode': 'lines+markers',
+        'marker': {'size': 3, 'color': 'turquoise'},
+        'line': {'color': 'turquoise'},
+        'name': "Frecuencia Fundamental",
+        'hovertemplate': 'Tiempo: %{x: .2f} s<br>Frecuencia: %{y: .2f} Hz'
+    }]
+
+    # Curva de intensidad (dB)
+    trace_intensity = [{
+        'x': intensity.xs().tolist(),  # Convertir a lista para ser serializable
+        'y': intensity.values.T.flatten().tolist(),  # Convertir a lista para ser serializable
+        'mode': 'lines',
+        'line': {'color': 'purple'},
+        'name': "Intesidad",
+        'yaxis': "y2",
+        'hovertemplate': 'Tiempo: %{x: .2f} s<br>Intensidad: %{y: .2f} dB'
+
+    }]
+
+    # Layout del gráfico con dos ejes y
+    layout = {
+        'title': "Frecuencia fundamental e Intensidad",
+        'xaxis': {'title': "Tiempo [s]"},
+        'yaxis': {'title': "Frecuencia [Hz]", 'range': [0, 1000]},
+        'yaxis2': {'title': "Intesidad [dB]", 'overlaying': 'y', 'side': 'right', 'range': [0, 120]},
+        'hovermode': 'closest',
+        'legend': {'x': 0.5, 'y': -0.2, 'xanchor': 'center', 'yanchor': 'top', 'orientation': 'h', 'font': {'size': 10}},
+        # 'margin': {'l': 50, 'r': 50, 't': 50, 'b': 50},
+        'autosize': True,
+        'dragmode': False 
+
+    }
+
+    # Combinar los datos de las trazas
+    traces = trace_pitch + trace_intensity
+ 
+    return traces, layout
+ 
+def draw_power_spectrum(frequencies, power):
+    power = power.flatten()
+   
+    # Verificar que frequencies y power tengan la misma longitud
+    # if len(frequencies) != len(power):
+    #     min_length = min(len(frequencies), len(power))
+    #     frequencies = frequencies[:min_length]
+    #     power = power[:min_length]
+   
+    # valid_idx = np.isfinite(power)
+    # frequencies = frequencies[valid_idx]
+    # power = power[valid_idx]
+ 
+    # smoothed_power = savgol_filter(power, window_length=101, polyorder=2)
+    trace_spectrum = [{
+        'x': frequencies.tolist(),
+        'y': power.tolist(),
+        'mode': 'lines',
+        'line': {'color': 'blue', 'width': 3},
+        'name': "Espectro",
+        'hovertemplate': 'Frecuencia: %{x: .2f} Hz<br>Intensidad: %{y: .2f} dB'
+
+    }]
+ 
+    layout_spectrum = {
+        'title': "Espectro de Potencia",
+        'xaxis': {'title': "Frecuencia [Hz]", 'range': [0, 8000]},
+        'yaxis': {'title': "Intesidad [dB]"},
+        'hovermode': 'closest',
+        'autosize': True,
+        'dragmode': False 
+
+    }
+
+ 
+    return trace_spectrum, layout_spectrum
+ 
+def draw_waveform(time, amplitude):
+
+    #  Datos del oscilograma
+    trace_oscilogram = [{
+        'x': time.tolist(),  
+        'y': amplitude.tolist(),  
+        'mode': 'lines',
+        'line': {'color': 'black'},
+        'name': 'Oscilograma',
+        'hovertemplate': 'Tiempo: %{x: .2f} s<br>Amplitud: %{y: .2f}'
+    }]
+
+    layout_oscilogram = {
+        'title': "Oscilograma",
+        'xaxis': {'title': "Tiempo [s]"},
+        'yaxis': {'title': "Amplitud",  'range': [-1, 1]},
+        'hovermode': 'closest',
+        'legend': {'x': 0.5, 'y': -0.2, 'xanchor': 'center', 'yanchor': 'top', 'orientation': 'h'
+        },
+        # 'margin': {'l': 50, 'r': 50, 't': 50, 'b': 50},
+        'autosize': True,
+        'dragmode': False 
+
+    }
+ 
+    return trace_oscilogram, layout_oscilogram
+ 
+def generate_text_file(time_pitch, pitch_values, intensity, formants_values, spectrogram_data):
+    """
+    Genera un archivo de texto con los datos de pitch, intensidad y formantes,
+    alineandos en tiempo.
+    """
+    output = io.StringIO()
+    output.write("{:<10}/\t{:<15}\t/\t{:<15}\t/\t{:<15}\t/\t{:<15}\t/\t{:<15}\n".format(
+        "Tiempo [s]", "Frecuencia [Hz]", "Intesidad [dB]", "Formante 1 [Hz]", "Formante 2 [Hz]", "Formante 3 [Hz]"))
+
+     # Los tiempos del análisis de pitch
+    pitch_frequencies = pitch_values
+    
+    # Extraer los tiempos e intensidades
+    times_intensity = intensity.xs()
+    intensity_values = intensity.values.T.flatten()
+
+    # Interpolar la intensidad para que coincida con los tiempos de pitch
+    intensity_interpolated = np.interp(time_pitch, times_intensity, intensity_values)
+
+    # Crear listas para los formantes
+    formant1 = formants_values[0]
+    formant2 =formants_values [1]
+    formant3 = formants_values[2]
+
+
+    # Asegurarnos de que todos los datos tengan el mismo tamaño
+
+    min_length = min(len(time_pitch), len(pitch_frequencies), len(intensity_interpolated), len(formant1), len(formant2), len(formant3))
+
+    # Escribir los datos alineados al archivo
+    for i in range(min_length):
+        output.write("{:<10.4f}/\t{:<15.2f}\t/\t{:<15.2f}\t/\t{:<15.2f}\t/\t{:<15.2f}\t/\t{:<15.2f}\n".format(
+            time_pitch[i], pitch_frequencies[i], intensity_interpolated[i], 
+            formant1[i], formant2[i], formant3[i]))
+
+
+    text_content = output.getvalue()
+    output.close()
+   
+    return text_content
+    #  Crear el buffer de StringIO para escribir los datos en memoria
+    # output = io.StringIO()
+
+    # # Escribir el encabezado
+    # output.write("{:<10}/\t{:<15}\t/\t{:<15}\n".format(
+    #     "Time [s]","Frequency [Hz]", "Power [dB]"))
+    
+    # times = spectrogram_data['times']
+    # frequencies = spectrogram_data['frequencies']
+    # power_values = spectrogram_data['power_values']  # Matriz Z
+
+    # # Iterar sobre los tiempos y frecuencias para escribir los valores en el buffer
+    # for i, time in enumerate(times):
+    #     for j, freq in enumerate(frequencies):
+    #         # Escribir tiempo, frecuencia y potencia correspondientes en el buffer
+    #         output.write("{:<10.4f}/\t{:<15.2f}\t/\t{:<15.2f}\n".format(
+    #             time, freq, power_values[j][i]))
+
+    # # Obtener el contenido de texto del StringIO
+    # text_content = output.getvalue()
+
+    # # Cerrar el buffer
+    # output.close()
+
+    # # Devolver el contenido de texto para guardarlo o manipularlo
+    # return text_content
+
+
+def analyze_audio(snd, live):
+    snd_normalize = snd.copy()
+    max_amplitude = np.max(np.abs(snd.values))  # Obtener el valor máximo de la señal
+    if max_amplitude > 0:
+        normalized_values = snd.values / max_amplitude 
+    else:
+        normalized_values = snd.values  
+    snd_normalize.values = normalized_values 
+
+    # Generar el oscilograma con Plotly
+    time = snd_normalize.xs()
+    amplitude = snd_normalize.values.flatten()
+    trace_oscilogram, layout_oscilogram = draw_waveform(time, amplitude)
+
+    # with open('oscilogram_male.txt', 'w') as archivo:
+    #     archivo.write("{:<10}/\t{:<15}\n".format("Time [s]", "Amplitud"))
+    
+    # # Escribir los valores de tiempo y amplitud
+    #     for t, a in zip(time, amplitude):
+    #         archivo.write("{:<10}/\t{:<15}\n".format(t,a))
+
+    # Análisis de Pitch (frecuencia fundamental)
+    pitch = snd_normalize.to_pitch()
+    pitch_values = pitch.selected_array['frequency']
+    # pitch_values[pitch_values == 0] = np.nan 
+    time_pitch = pitch.xs() # Reemplazar partes no sonoras con NaN
+    # Análisis de Formantes usando LPC (método Burg)
+    formants = snd_normalize.to_formant_burg()
+
+    # Generar el espectrograma 2D con Plotly
+    trace_spectrogram, layout_spectrogram, formants_values, spectrogram_data  = draw_spectrogram(snd.to_spectrogram(window_length=0.1, maximum_frequency=8000), pitch_values, time_pitch, formants)
+
+    # Análisis de Intensidad
+    intensity = snd_normalize.to_intensity()
+
+    # Generar la gráfica combinada de pitch e intensidad
+    trace_intensity, layout_intensity = draw_combined_pitch_intensity_contour(pitch_values, time_pitch, intensity)
+
+
+    if not live:
+        mean_pitch = call(pitch, "Get mean", 0, 0, "Hertz")
+ 
+        # Generar el espectrograma 3D con Plotly
+        trace_spectrogram_3d, layout_spectrogram_3d = draw_spectrogram_3d(snd.to_spectrogram(window_length=0.1, maximum_frequency=8000))
+ 
+        # Generar el espectro de potencia con Plotly
+        spectrum = snd.to_spectrum()
+        frequencies = spectrum.xs()
+        power = np.where(spectrum.values.T > 0, 10 * np.log10(spectrum.values.T), 0)
+        peak_frequency = frequencies[np.nanargmax(power)]
+        print(f"Frecuencia del pico más alto: {peak_frequency} Hz")
+        trace_spectrum, layout_spectrum = draw_power_spectrum(frequencies, power)
+ 
+        # Generar el archivo de texto con los datos
+        text_content = generate_text_file(time_pitch, pitch_values, intensity, formants_values, spectrogram_data)
+        data_to_send = json.dumps({
+            'trace_oscilogram': trace_oscilogram,
+            'layout_oscilogram': layout_oscilogram,
+            'trace_spectrogram': trace_spectrogram,
+            'layout_spectrogram': layout_spectrogram,
+            'trace_intensity': trace_intensity,
+            'layout_intensity': layout_intensity,
+            'trace_spectrogram_3d': trace_spectrogram_3d,
+            'layout_spectrogram_3d': layout_spectrogram_3d,
+            'trace_spectrum': trace_spectrum,
+            'layout_spectrum': layout_spectrum, 
+            'text_content': text_content,
+            'spectrogram_data': spectrogram_data
+        })
+        # data_to_send_git =({
+        #     'trace_oscilogram': trace_oscilogram,
+        #     'layout_oscilogram': layout_oscilogram,
+        #     'trace_spectrogram': trace_spectrogram,
+        #     'layout_spectrogram': layout_spectrogram,
+        #     'trace_intensity': trace_intensity,
+        #     'layout_intensity': layout_intensity,
+        #     'trace_spectrogram_3d': trace_spectrogram_3d,
+        #     'layout_spectrogram_3d': layout_spectrogram_3d,
+        #     'trace_spectrum': trace_spectrum,
+        #     'layout_spectrum': layout_spectrum,
+        # })
+        # with open('data.json', 'w') as json_file:
+        #     json.dump(data_to_send_git,json_file)
+
+        return data_to_send
+    else:
+        data_to_send = json.dumps({
+            'trace_oscilogram': trace_oscilogram,
+            'layout_oscilogram': layout_oscilogram,
+            'trace_spectrogram': trace_spectrogram,
+            'layout_spectrogram': layout_spectrogram,
+            'trace_intensity': trace_intensity,
+            'layout_intensity': layout_intensity
+        })
+        return data_to_send
+
+
